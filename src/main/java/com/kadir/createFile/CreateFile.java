b022a5ac4a630fcba19d4aec24405a8a0f640b7b
kadiryalcin <ygulsun.sahin@gmail.com>
2022-05-08 15:05:22 +0200

    new examples.

--- src/main/java/com/kadir/createFile/CreateFile.java
+++ src/main/java/com/kadir/createFile/CreateFile.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class CreateFile {
-
     public static void main(String[] args) throws IOException {
 
         File file = new File("TestFile3.txt");
